7892a143c2980494bd75301fc36a48cb20c51683
Thais Helena Moretto <CIANDT\thaism@lnb028293cps.ciandt.global>
2022-12-29 17:19:12 -0300

    Cria autenticação de usuário, rota de autenticação, middleware para verificação de validade do token e AppError

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../erros/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing!", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: userId } = verify(
+      token,
+      "cd7b7cff97011353c3b56fb8df36d4ad374ad28b",
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError("User does ot exists!", 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError("Invalid token!", 401);
+  }
+}
---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../erros/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const token = sign({}, "cd7b7cff97011353c3b56fb8df36d4ad374ad28b", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
