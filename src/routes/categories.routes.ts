2d06c0a15fc68281743a4d27f46b5cbccb26d4ae
Thais Helena Moretto <CIANDT\thaism@lnb028293cps.ciandt.global>
2022-12-30 12:00:49 -0300

    Cria configuração de upload de arquivos e modifica forma de utilizar nas rotas

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
 
+import uploadConfig from "../config/upload";
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "../modules/cars/useCases/ImportCategory/ImportCategoryController";
 import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
 
-const upload = multer({ dest: "./tmp" });
+const uploadFile = multer(uploadConfig.upload("./tmp"));
 
 const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
@@ -19,7 +20,7 @@ categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post(
   "/import",
-  upload.single("file"),
+  uploadFile.single("file"),
   importCategoryController.handle,
 );
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
+
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/updateUserAvatarController";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserControler = new CreateUserController();
+const updateUserAvatarControler = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserControler.handle);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarControler.handle,
+);
 
 export { usersRoutes };
