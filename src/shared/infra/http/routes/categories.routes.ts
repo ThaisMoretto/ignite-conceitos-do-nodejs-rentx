415d923ce9afe4839f3bc91f9ce493ecd891f4a3
Thais Helena Moretto <CIANDT\thaism@lnb028293cps.ciandt.global>
2023-01-07 17:16:18 -0300

    Cria middleware de administrador e utiliza nas rotas necessárias

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -6,6 +6,9 @@ import { CreateCategoryController } from "@modules/cars/useCases/createCategory/
 import { ImportCategoryController } from "@modules/cars/useCases/ImportCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
+
 const categoriesRoutes = Router();
 
 const uploadFile = multer(uploadConfig.upload("./tmp"));
@@ -14,13 +17,20 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle,
+);
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post(
   "/import",
   uploadFile.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle,
 );
 
